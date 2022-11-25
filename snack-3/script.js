e4a3073bc25533bf14314f6326c1df9b25b96a6c
MatteoCarbone17 <matteocarbone17@live.com>
2022-11-25 16:52:01 +0100

    snack-3 + bonus

---
+++ snack-3/script.js
@@ -0,0 +1,20 @@
+/* Calcola la somma dei primi 10 numeri di un array.*/
+   
+/*Calcola la somma e la media dei primi 10 numeri di un array */
+
+
+let listNumbers = [7 , 10, 2 , 4 , 6 , 10 , 16545, 5 , 6 , 6 , 6 , 5 , 67 , 65 , 76 ,323,  6 , 6 , 5 , 67 , 65 , 76 ,323 ];
+
+let total = 0;
+
+for (let i = 0; i <= 10; i++) {
+
+total += listNumbers[i];
+
+media = total % listNumbers[i] ;
+
+};
+
+console.log(total);
+
+console.log(media);
