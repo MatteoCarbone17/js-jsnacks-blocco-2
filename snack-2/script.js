ab79a2d238a29209ea1b592dcc9750c3f61730af
MatteoCarbone17 <matteocarbone17@live.com>
2022-11-25 16:06:22 +0100

    snack-2-fix

--- snack-2/script.js
+++ snack-2/script.js
@@ -6,8 +6,10 @@ let userNumber = parseInt(prompt('scegli un numero'));
 
 
 if(userNumber % 2 == 0 ){
-
   console.log(userNumber);
+
 } else if ( userNumber % 2 == 1){
-  console.log(userNumber++)
+
+  userNumber++;
+  console.log(userNumber)
 }
