760219c5c29e11228ba73201e35d35de93adc31e
MatteoCarbone17 <matteocarbone17@live.com>
2022-11-28 12:47:37 +0100

    snack-6

---
+++ snack-6/script.js
@@ -0,0 +1,36 @@
+
+// Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari //
+
+
+let listNumbers = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+];
+
+
+let sommaDispari = 0;
+
+for (let i = 0; i <= listNumbers.length; i++) {
+
+    if (sommaDispari = listNumbers[i] % 2 == 1 ){
+        
+        sommaDispari *= listNumbers[i]
+        console.log(sommaDispari)
+
+    } 
+    };
+
+    
