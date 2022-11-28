087b2f6b83cbb68b59f549b9047a5ad7e4401d10
MatteoCarbone17 <matteocarbone17@live.com>
2022-11-28 10:38:08 +0100

    First monday snack-4

--- snack-3/script.js
+++ snack-3/script.js
@@ -3,6 +3,8 @@
 /*Calcola la somma e la media dei primi 10 numeri di un array */
 
 
+/*Calcola la somma, la media e il valore massimo dei primi 10 numeri di un array.*/
+
 let listNumbers = [7 , 10, 2 , 44 , 62 , 10 , 16, 5 , 6 , 6 , 6 , 5 , 67 , 65 , 76 ,323,  6 , 6 , 5 , 67 , 65 , 76 ,323 ];
 
 let total = 0;
@@ -10,10 +12,14 @@ let total = 0;
 for (let i = 0; i <= 10; i++) {
 
 total += listNumbers[i];
+ 
+ let maxVal =  ;
+ console.log(maxVal);
 
 };
 
 let media = total / 10;
+
 
 console.log(total);
 
---
+++ snack-4/script.js
@@ -0,0 +1,9 @@
+// stampa le potenze di 2 fino  a 1000//
+
+
+let numero = 2 ;
+
+while ( numero < 1000){
+    numero = numero * 2;
+    console.log(numero);
+};
